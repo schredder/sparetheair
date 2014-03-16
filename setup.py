a695f45834fb46f215aa01c6e88e820c75245b5c
Eric Schroeder <eschroeder@ebay.com>
2014-03-16 14:29:26 -0700

    added setup.py and requirements.txt for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SpareTheAir',
+		version='1.0',
+		description='Spare The Air iCal generator',
+		author='Eric Scroeder',
+		author_email='eric@eschro.com',
+		url='https://github.com/schredder/sparetheair',
+		py_modules=['sparetheair', 'sparetheair.test']
+)
